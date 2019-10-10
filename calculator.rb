9ca80cf8bfa7afd101fb698d56332d7d8bc72ab1
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-10-10 11:58:33 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,36 @@
 # Add your variables here
+
+def first_number = "0"
+puts "0"
+end 
+
+def second_number = "1"
+  puts "1"
+end 
+
+def sum = "first_number+second_number"
+  puts "0+1"
+end 
+
+def difference = "first_number - second_number"
+  puts "0-1"
+end
+
+def product = "first_number * second_number"
+  puts "0*1"
+end 
+
+def quotient = "first_number / second_number"
+  puts "0/1"
+end 
+
+
+
+
+
+
+
+
+
+
+
